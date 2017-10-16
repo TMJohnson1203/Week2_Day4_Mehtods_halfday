7449e36c2bed6a9641c50af550da67355ac0d350
TMJohnson1203 <tmjohnson1203@gmail.com>
2017-10-16 15:11:10 -0400

    initial commit

---
+++ Week2_Day4_Mehtods_halfday/Cat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_Day4_Mehtods_halfday
+{
+    
+    {
+        static void Main
+
+    {
+        static void Main(string[] args)
+    {
+
+        Cat mittens = new Cat();
+        mittens.nAME = "Mittens";
+    }
+}
+}
---
+++ Week2_Day4_Mehtods_halfday/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_Day4_Mehtods_halfday
+{
+    class cat
+    {
+        static void Main(string[] args)
+        {
+            {
+                static void Main(string[] args)
+        {
+
+                    Cat mittens = new Cat();
+                    mittens.Name = "Mittens";
+                }
+
+
+                //public static double monthlyWage(double hoursWorked, double hourlyWage)     // method decaration // monthlyWage = method name// 
+                //{ // PARAMETERS are in parentheses
+
+                //    double monthlyWage = (double hoursWorked,  double hourlyWage); 
+
+                //    // method decaration // monthlyWage = method name// 
+                //    double monthlyWage = hoursWorked * hourlyWage;
+                //    return mothlyWage;
+                //}
+
+                //{Console.WriteLine("Please enter the hours you have worked");
+
+                //Console.WriteLine("Please enter your hourly wage.");
+
+                //Console.WriteLine("Your monthly wage is " + (double monthlyWage));
+                //}
+
+
+                ////Create a method called WageCalculator.
+
+                //// It should take two double parameters. One should represent the hours worked and the other represent the hourly wage.The return type should be of type of double.
+                ////monthlyWage
+                //Console.WriteLine("Please enter the hours you have worked");
+                //    //double worked = double.Parse(Console.ReadLine);
+
+
+                //    Console.WriteLine("Please enter your hourly wage.");
+
+                //    Console.WriteLine(monthlyWage)
+
+                //    //int hoursWorked;
+                //    //int hourlyWage;
+                //    //Console.ReadLine()
+                //    //Console.WriteLine("Your monthly wage is );     ////The method should calculate the monthly wage.
+                ////Once it calculates the monthly wage it should print Your monthly wage is X to the console where 'X' represents the value that was calculated.
+
+
+
+
+            }
+}
+}
